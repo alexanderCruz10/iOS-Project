4c97e4ee6b6d2f30a7b498b67caffb4065f9a3cc
Alex Cruz <alexcruz970@yahoo.ca>
2022-10-03 22:36:20 -0400

    Refactored: Code refactored to make it generic for displaying onboarding

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.backgroundColor = .systemBackground
-        window?.rootViewController = OnboardingViewController()//OnboardingContainerViewController() //LoginViewController()
+        //window?.backgroundColor = .systemBackground
+        window?.rootViewController = OnboardingContainerViewController() //LoginViewController()
         
         return true
     }
